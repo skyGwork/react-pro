15956a44683c00418078815a2da712731aafc8b8
skyGwork <skyGworkk@gmail.com>
2022-08-18 13:26:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,47 +1,39 @@
 import React from 'react';
 
-const firstBook = {
-  img: 'https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg',
-  title: 'The Psychology of Money',
-  auther: 'Morgan Housel',
-};
-const secondBook = {
-  img: 'https://images-eu.ssl-images-amazon.com/images/I/71sBtM3Yi5L._AC_UL604_SR604,400_.jpg',
-  title: 'Subconscious Mind',
-  auther: 'Joseph Murphy',
-};
+const books = [
+  {
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg',
+    title: 'The Psychology of Money',
+    auther: 'Morgan Housel',
+  },
+  {
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/71sBtM3Yi5L._AC_UL604_SR604,400_.jpg',
+    title: 'Subconscious Mind',
+    auther: 'Joseph Murphy',
+  },
+  {
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg',
+    title: 'The Psychology of Money',
+    auther: 'Morgan Housel',
+  },
+];
+
 function App() {
   return (
     <section className="book_section">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        auther={firstBook.auther}
-      >
-        <span>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam
-          saepe sunt ex exercitationem optio provident nobis eius a veniam
-          obcaecati perspiciatis, quasi, maxime non cum minus? Facere, officiis
-          ipsa.
-        </span>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        auther={secondBook.auther}
-      />
+      {books.map((book) => {
+        return <Book next={book} />;
+      })}
     </section>
   );
 }
 const Book = (props) => {
-  const { img, title, auther, children } = props;
+  const { img, title, auther } = props.next;
   return (
     <article className="book">
       <img src={img} alt="book" />
       <h4>{title} </h4>
       <p>{auther}</p>
-      {children}
     </article>
   );
 };
