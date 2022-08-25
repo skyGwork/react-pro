6f00006628f665a6f02da602637a788921c34047
skyGwork <skyGworkk@gmail.com>
2022-08-26 02:28:35 +0530

    update

--- src/model/bookData.js
+++ src/model/bookData.js
@@ -1,13 +1,13 @@
 export const books = [
   {
     id: 1,
-    img: 'https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg',
+    img: 'https://santosh-test-demo-assets.s3.ap-south-1.amazonaws.com/A01.PNG',
     title: 'The Psychology of Money',
     auther: 'Morgan Housel',
   },
   {
     id: 2,
-    img: 'https://images-eu.ssl-images-amazon.com/images/I/71sBtM3Yi5L._AC_UL604_SR604,400_.jpg',
+    img: 'https://santosh-test-demo-assets.s3.ap-south-1.amazonaws.com/contactUs.PNG',
     title: 'Subconscious Mind',
     auther: 'Joseph Murphy',
   },
